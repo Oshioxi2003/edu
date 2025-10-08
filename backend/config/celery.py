bb2a150b5dc6e953451c9f14b40ee5a536f2fa4d
Oshioxi2003 <toanwa1@gmail.com>
2025-10-08 23:26:35 +0700

    feat: add backend/config/celery.py - version 1.0

---
+++ backend/config/celery.py
@@ -0,0 +1,22 @@
+"""
+Celery configuration.
+"""
+import os
+from celery import Celery
+
+# Set the default Django settings module
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+app = Celery('edu_platform')
+
+# Load configuration from Django settings
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# Auto-discover tasks from all installed apps
+app.autodiscover_tasks()
+
+
+@app.task(bind=True)
+def debug_task(self):
+    print(f'Request: {self.request!r}')
+
