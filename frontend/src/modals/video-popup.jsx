2dd47f29691cd261b8f44255ca2959eb56f6bfd0
Oshioxi2003 <toanwa1@gmail.com>
2025-10-09 22:56:15 +0700

    feat: add frontend/src/modals/video-popup.jsx - version 1.0

--- frontend/src/modals/video-popup.jsx
+++ frontend/src/modals/video-popup.jsx
@@ -1,5 +1,7 @@
+'use client';
+
 import React from "react";
-import ModalVideo from "react-modal-video";
+import Modal from "@/components/ui/Modal";
 
 const VideoPopup = ({
   isVideoOpen,
@@ -7,15 +9,24 @@ const VideoPopup = ({
   videoId = "d8w5SICzzxc",
 }) => {
   return (
-    <>
-      <ModalVideo
-        channel="youtube"
-        autoplay
-        isOpen={isVideoOpen}
-        videoId={videoId}
-        onClose={() => setIsVideoOpen(false)}
-      />
-    </>
+    <Modal
+      isOpen={isVideoOpen}
+      onClose={() => setIsVideoOpen(false)}
+      size="xl"
+    >
+      <div className="aspect-video w-full">
+        <iframe
+          width="100%"
+          height="100%"
+          src={`https://www.youtube.com/embed/${videoId}?autoplay=1`}
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+          className="rounded-lg"
+        ></iframe>
+      </div>
+    </Modal>
   );
 };
 
