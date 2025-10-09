f6eacfd5cba5dd042c01fcd8691c82418cc782a5
Oshioxi2003 <toanwa1@gmail.com>
2025-10-09 22:56:22 +0700

    feat: add frontend/src/components/ui/index.js - version 1.0

---
+++ frontend/src/components/ui/index.js
@@ -0,0 +1,10 @@
+export { default as Button } from './Button';
+export { default as Card, CardHeader, CardBody, CardFooter } from './Card';
+export { default as Badge } from './Badge';
+export { default as ProgressBar } from './ProgressBar';
+export { default as Input } from './Input';
+export { default as Skeleton, SkeletonCard, SkeletonList } from './Skeleton';
+export { default as Modal } from './Modal';
+export { default as ThemeSwitcher } from './ThemeSwitcher';
+export { default as LanguageSwitcher } from './LanguageSwitcher';
+
